7573794ccb794336ba041fc15a96307b8c31cf80
Max Obrist <obrist.max@gmail.com>
2021-03-05 19:41:32 +0100

    removed some useless factory methods

--- ResultZ/Results/Result.cs
+++ ResultZ/Results/Result.cs
@@ -4,31 +4,23 @@ namespace ResultZ
 {
     public static class Result
     {
-        public static IResult Pass() => new Passed();
         public static IResult Pass(params IReason[] reasons) => new Passed(reasons);
         public static IResult Pass(IEnumerable<IReason> reasons) => new Passed(reasons);
-        public static IResult Pass(string message) => new Passed(message);
         public static IResult Pass(string message, params IReason[] reasons) => new Passed(message, reasons);
         public static IResult Pass(string message, IEnumerable<IReason> reasons) => new Passed(message, reasons);
 
-        public static IResult<TValue> Pass<TValue>(TValue value) => new Passed<TValue>(value);
         public static IResult<TValue> Pass<TValue>(TValue value, params IReason[] reasons) => new Passed<TValue>(value, reasons);
         public static IResult<TValue> Pass<TValue>(TValue value, IEnumerable<IReason> reasons) => new Passed<TValue>(value, reasons);
-        public static IResult<TValue> Pass<TValue>(TValue value, string message) => new Passed<TValue>(value, message);
         public static IResult<TValue> Pass<TValue>(TValue value, string message, params IReason[] reasons) => new Passed<TValue>(value, message, reasons);
         public static IResult<TValue> Pass<TValue>(TValue value, string message, IEnumerable<IReason> reasons) => new Passed<TValue>(value, message, reasons);
 
-        public static IResult Fail() => new Failed();
         public static IResult Fail(params IReason[] reasons) => new Failed(reasons);
         public static IResult Fail(IEnumerable<IReason> reasons) => new Failed(reasons);
-        public static IResult Fail(string message) => new Failed(message);
         public static IResult Fail(string message, params IReason[] reasons) => new Failed(message, reasons);
         public static IResult Fail(string message, IEnumerable<IReason> reasons) => new Failed(message, reasons);
 
-        public static IResult<TValue> Fail<TValue>() => new Failed<TValue>();
         public static IResult<TValue> Fail<TValue>(params IReason[] reasons) => new Failed<TValue>(reasons);
         public static IResult<TValue> Fail<TValue>(IEnumerable<IReason> reasons) => new Failed<TValue>(reasons);
-        public static IResult<TValue> Fail<TValue>(string message) => new Failed<TValue>(message);
         public static IResult<TValue> Fail<TValue>(string message, params IReason[] reasons) => new Failed<TValue>(message, reasons);
         public static IResult<TValue> Fail<TValue>(string message, IEnumerable<IReason> reasons) => new Failed<TValue>(message, reasons);
     }
