4250b649d6fa4b2cb3eeaecbbc553e6cfac75232
Max Obrist <obrist.max@gmail.com>
2021-03-13 23:46:45 +0100

    Some adaption of WithSuccess/ErrorIf overloads. +semver: minor

--- ResultZ/Extensions/ResultExtensions.WithErrorIf.cs
+++ ResultZ/Extensions/ResultExtensions.WithErrorIf.cs
@@ -1,28 +1,44 @@
-﻿namespace ResultZ
+﻿using System;
+
+namespace ResultZ
 {
-    // TODO: needs overloads that return IResult<TValue> to keep consistency
+    // TODO: Should have the same overloads as ResultBuilder
     public static partial class ResultExtensions
     {
-        public static IResult WithErrorIf<TError>(this IResult result, bool condition, TError error)
-            where TError : Error
+        public static IResult WithErrorIf(this IResult result, bool condition, Error error)
             => condition ? result.WithError(error) : result;
 
-        public static IResult WithErrorIf<TError>(this IResult result, bool condition)
-            where TError : Error, new()
-            => result.WithErrorIf(condition, new TError());
-
-        // TODO: errors should be created using an action. Otherwise they need to be evaluated before passing, leading to ugly nullability issues (and similiar stuff)
-        public static IResult WithErrorIf<TError, TSuccess>(this IResult result,
-                                                       bool condition,
-                                                       TError error,
-                                                       TSuccess orSuccess)
-            where TError : Error
-            where TSuccess : Success
+        public static IResult WithErrorIf(this IResult result,
+                                          bool condition,
+                                          Error error,
+                                          Success orSuccess)
             => condition ? result.WithError(error) : result.WithSuccess(orSuccess);
 
-        public static IResult WithErrorIf<TError, TSuccess>(this IResult result, bool condition)
-            where TError : Error, new()
-            where TSuccess : Success, new()
-            => result.WithErrorIf(condition, new TError(), new TSuccess());
+        public static IResult<TValue> WithErrorIf<TValue>(this IResult<TValue> result, bool condition, Error error)
+            => condition ? result.WithError(error) : result;
+
+        public static IResult<TValue> WithErrorIf<TValue>(this IResult<TValue> result,
+                                                          bool condition,
+                                                          Error error,
+                                                          Success orSuccess)
+            => condition ? result.WithError(error) : result.WithSuccess(orSuccess);
+
+        public static IResult WithErrorIf(this IResult result, bool condition, Func<Error> error)
+            => condition ? result.WithError(error()) : result;
+
+        public static IResult WithErrorIf(this IResult result,
+                                          bool condition,
+                                          Func<Error> error,
+                                          Func<Success> orSuccess)
+            => condition ? result.WithError(error()) : result.WithSuccess(orSuccess());
+
+        public static IResult<TValue> WithErrorIf<TValue>(this IResult<TValue> result, bool condition, Func<Error> error)
+            => condition ? result.WithError(error()) : result;
+
+        public static IResult<TValue> WithErrorIf<TValue>(this IResult<TValue> result,
+                                                          bool condition,
+                                                          Func<Error> error,
+                                                          Func<Success> orSuccess)
+            => condition ? result.WithError(error()) : result.WithSuccess(orSuccess());
     }
 }
