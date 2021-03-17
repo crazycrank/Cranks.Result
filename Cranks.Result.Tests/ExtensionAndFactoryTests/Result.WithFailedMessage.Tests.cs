d63fc1cc4bc84ac740d70cd18aefb6f115f00606
Max Obrist <obrist.max@gmail.com>
2021-03-17 18:34:35 +0100

    Add WithFailedMessage and WithPassedMessage

---
+++ Cranks.Result.Tests/ExtensionAndFactoryTests/Result.WithFailedMessage.Tests.cs
@@ -0,0 +1,49 @@
+﻿using Shouldly;
+
+using Xunit;
+
+namespace Cranks.Result.Tests.ExtensionAndFactoryTests
+{
+    public class ResultWithFailedMessageTests
+    {
+        [Fact]
+        public void WithFailedMessage_IsFailed_NowHasMessage()
+        {
+            var result = Result.Fail()
+                               .WithMessage("messageOld")
+                               .WithFailedMessage("messageNew");
+
+            result.Message.ShouldBe("messageNew");
+        }
+
+        [Fact]
+        public void WithFailedMessage_IsPassed_RetainsOldMessage()
+        {
+            var result = Result.Pass()
+                               .WithMessage("messageOld")
+                               .WithFailedMessage("messageNew");
+
+            result.Message.ShouldBe("messageOld");
+        }
+
+        [Fact]
+        public void WithFailedMessage_IsFailed_WithOrMessage_NowHasMessage()
+        {
+            var result = Result.Fail()
+                               .WithMessage("messageOld")
+                               .WithFailedMessage("messageNew", "messageOr");
+
+            result.Message.ShouldBe("messageNew");
+        }
+
+        [Fact]
+        public void WithFailedMessage_IsPassed_WithOrMessage_NowHasMessage()
+        {
+            var result = Result.Pass()
+                               .WithMessage("messageOld")
+                               .WithFailedMessage("messageNew", "messageOr");
+
+            result.Message.ShouldBe("messageOr");
+        }
+    }
+}
